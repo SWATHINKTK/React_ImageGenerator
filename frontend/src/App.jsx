c91725e5226c4707537320551057d221ee7bd8b5
SWATHINKTK <swathinktk10@gmail.com>
2024-02-11 08:46:54 +0530

    usermangement system.login page design is completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './Components/login/Login'
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/login/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import style from './login.module.css';
+
+function Login() {
+  return (
+    <div className={style.login_page}>
+      <div className={style.container}>
+            <div className={style.left_container}>
+              <img className={style.login_left_image} src="../../public/Images/BG2.avif" alt="" />
+            </div>
+            <div className={style.right_container}>
+                <div>
+                  <h2>Login Page</h2>
+                  <p>hey enter your details to sign in your account ...!</p>
+                </div>
+                <form action="" className={style.login_form}>
+                  <label htmlFor="username">Username</label>
+                  <input type="text" placeholder='username'/>
+                  <label htmlFor="password" >Password</label>
+                  <input type="text"  placeholder='password' />
+                  <button className={style.login_btn}>Login</button>
+                </form>
+                <button className={style.signup_btn}>Create New Account</button>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
